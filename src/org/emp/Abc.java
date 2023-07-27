4b25946255c3b5f1fe20c72f97426449c474e460
Raajesh123 <rajesh.eie94@gmail.com>
2023-07-27 15:22:26 +0530

    rajesh

---
+++ src/org/emp/Abc.java
@@ -0,0 +1,25 @@
+package org.emp;
+
+public class Abc {
+	private void m5() {
+		System.out.println("giri");
+			}
+	
+private void m7() {
+System.out.println("raaaajesh");
+	}
+	
+	private void m3() {
+		System.out.println("prabhu");
+			}
+	
+	private void m4() {
+		System.out.println("vimal");
+			}
+public static void main(String[] args) {
+	System.out.println("done");
+	
+	
+	
+}
+}
